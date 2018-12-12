5f8ee19e131286d9096fb1e717c370ba7562c053
Andrey Parvatkin <badas_github@mail.ru>
2018-12-12 21:40:35 +0400

    add list

--- lib/src/ui/movie_list.dart
+++ lib/src/ui/movie_list.dart
@@ -1,2 +1,44 @@
-library bloc_study.src.ui.movie_list;
+import 'package:bloc_study/src/models/item_model.dart';
+import 'package:flutter/material.dart';
 
+import '../blocs/movies_bloc.dart';
+
+class MovieList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    bloc.fetchAllMovies();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Popular Movies'),
+      ),
+      body: StreamBuilder(
+          stream: bloc.allMovies,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return _buildList(snapshot);
+            } else if (snapshot.hasError) {
+              return Text(snapshot.error.toString());
+            }
+
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }),
+    );
+  }
+
+  Widget _buildList(AsyncSnapshot<ItemModel> snapshot) {
+    return GridView.builder(
+      itemCount: snapshot.data.results.length,
+      gridDelegate:
+          new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+      itemBuilder: (BuildContext context, int index) {
+        return Image.network(
+          'https://image.tmdb.org/t/p/w185${snapshot.data.results[index].posterPath}',
+          fit: BoxFit.cover,
+        );
+      },
+    );
+  }
+}
