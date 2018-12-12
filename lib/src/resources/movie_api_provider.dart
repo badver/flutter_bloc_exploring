e4d90c5c59c9f4dc6b90a0739321e5acdf28ea3b
Andrey Parvatkin <badas_github@mail.ru>
2018-12-12 23:45:03 +0400

    add trailer to api

--- lib/src/resources/movie_api_provider.dart
+++ lib/src/resources/movie_api_provider.dart
@@ -1,19 +1,30 @@
 import 'dart:convert';
 
 import 'package:bloc_study/src/models/item_model.dart';
+import 'package:bloc_study/src/models/trailer_model.dart';
 import 'package:http/http.dart' show Client;
 
 class MovieApiProvider {
   Client client = Client();
+  final _baseUrl = "https://api.themoviedb.org/3/movie";
   final _apiKey = 'api_key_place_here'; // TODO put api key
 
   Future<ItemModel> fetchMovieList() async {
-    final response = await client
-        .get("https://api.themoviedb.org/3/movie/popular?api_key=$_apiKey");
+    final response = await client.get("$_baseUrl/popular?api_key=$_apiKey");
     if (response.statusCode == 200) {
       return ItemModel.fromJson(json.decode(response.body));
-    } else {
-      throw Exception('Failed to load data');
     }
+
+    throw Exception('Failed to load data');
+  }
+
+  fetchTrailer(int movieId) async {
+    final response =
+        await client.get("$_baseUrl/$movieId/videos?api_key=$_apiKey");
+    if (response.statusCode == 200) {
+      return TrailerModel.fromJson(json.decode(response.body));
+    }
+
+    throw Exception('Failed to load trailers');
   }
 }
