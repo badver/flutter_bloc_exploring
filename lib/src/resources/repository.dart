6c1e7bcb31132b17679f411193a2bdd243e1afb1
Andrey Parvatkin <badas_github@mail.ru>
2018-12-12 21:40:46 +0400

    add repository

---
+++ lib/src/resources/repository.dart
@@ -0,0 +1,8 @@
+import 'package:bloc_study/src/models/item_model.dart';
+import 'package:bloc_study/src/resources/movie_api_provider.dart';
+
+class Repository {
+  final movieApiProvider = MovieApiProvider();
+
+  Future<ItemModel> fetchAllMovies() => movieApiProvider.fetchMovieList();
+}
