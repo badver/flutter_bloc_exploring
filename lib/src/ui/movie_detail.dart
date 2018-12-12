b300f97fde7e62e9efa1874b07606892ad60e3c8
Andrey Parvatkin <badas_github@mail.ru>
2018-12-12 23:16:11 +0400

    add detail view

---
+++ lib/src/ui/movie_detail.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+class MovieDetail extends StatefulWidget {
+  final int movieId;
+  final String title;
+  final posterUrl;
+  final description;
+  final releaseDate;
+  final String voteAverage;
+
+  MovieDetail(
+      {this.title,
+      this.posterUrl,
+      this.description,
+      this.releaseDate,
+      this.voteAverage,
+      this.movieId});
+
+  @override
+  State<StatefulWidget> createState() {
+    return MovieDetailState(
+        title: title,
+        posterUrl: posterUrl,
+        description: description,
+        releaseDate: releaseDate,
+        voteAverage: voteAverage,
+        movieId: movieId);
+  }
+}
+
+class MovieDetailState extends State<MovieDetail> {
+  final posterUrl;
+  final description;
+  final releaseDate;
+  final String title;
+  final String voteAverage;
+  final int movieId;
+
+  MovieDetailState(
+      {this.title,
+      this.posterUrl,
+      this.description,
+      this.releaseDate,
+      this.voteAverage,
+      this.movieId});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        top: false,
+        bottom: false,
+        child: NestedScrollView(
+            headerSliverBuilder:
+                (BuildContext context, bool innerBoxIsScrolled) {
+              return <Widget>[
+                SliverAppBar(
+                  expandedHeight: 200.0,
+                  floating: false,
+                  pinned: true,
+                  elevation: 0.0,
+                  flexibleSpace: FlexibleSpaceBar(
+                    background: Image.network(
+                      "https://image.tmdb.org/t/p/w500$posterUrl",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                )
+              ];
+            },
+            body: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(top: 5.0),
+                  ),
+                  Text(title,
+                      style: TextStyle(
+                          fontSize: 25.0, fontWeight: FontWeight.bold)),
+                  Container(margin: EdgeInsets.symmetric(vertical: 8.0)),
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.favorite,
+                        color: Colors.red,
+                      ),
+                      Container(margin: EdgeInsets.symmetric(horizontal: 1.0)),
+                      Text(
+                        voteAverage,
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 10.0),
+                      ),
+                      Text(
+                        releaseDate,
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                    ],
+                  ),
+                  Container(margin: EdgeInsets.symmetric(vertical: 8.0)),
+                  Text(description)
+                ],
+              ),
+            )),
+      ),
+    );
+  }
+}
