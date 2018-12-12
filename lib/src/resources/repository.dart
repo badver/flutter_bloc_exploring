a9281a38e5f5076038c6c70a260508b91b24885f
Andrey Parvatkin <badas_github@mail.ru>
2018-12-12 23:47:07 +0400

    add trailer to repository

--- lib/src/resources/repository.dart
+++ lib/src/resources/repository.dart
@@ -1,8 +1,12 @@
 import 'package:bloc_study/src/models/item_model.dart';
+import 'package:bloc_study/src/models/trailer_model.dart';
 import 'package:bloc_study/src/resources/movie_api_provider.dart';
 
 class Repository {
   final movieApiProvider = MovieApiProvider();
 
   Future<ItemModel> fetchAllMovies() => movieApiProvider.fetchMovieList();
+
+  Future<TrailerModel> fetchTrailers(int movieId) =>
+      movieApiProvider.fetchTrailer(movieId);
 }
